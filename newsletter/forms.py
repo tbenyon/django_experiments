ddcadebd1fd403d8e58438fc19067973277d7134
tbenyon <tom.benyon@gmail.com>
2016-03-30 18:00:40 +0100

    Add custom validation Sign Up Form e-mail field

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -6,3 +6,15 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
         fields = ['full_name', 'email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if len(email.split('@')) != 2:
+            raise forms.ValidationError('You should have one "@" in your address.')
+
+        base, hostname = email.split('@')
+        if hostname != "ths.uk.net":
+            raise forms.ValidationError('Please ensure you use a "ths.uk.net" e-mail address.')
+
+        return email
